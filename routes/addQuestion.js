3dfba667df23404d742b306fb1eccce4d489e7f9
Md Hishaam Akhtar <hishaamakhtar2001.mha@gmail.com>
2020-02-17 23:19:11 +0530

    Have to use await and promises. Please check

--- routes/addQuestion.js
+++ routes/addQuestion.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const Action = require("../model/Action");
 const verify = require("./verifyToken");
 
-router.post("/:action_id", verify, (req, res) => {
+router.post("/:action_id", verify, async (req, res) => {
   const ActionID = req.params.action_id;
   Action.findByIdAndUpdate(
     ActionID,
@@ -13,18 +13,14 @@ router.post("/:action_id", verify, (req, res) => {
         res.json(err);
       }
     }
-  ).then(() => {
-    Action.findById(ActionID)
-      .then(data => {
-        length = data.Questions.length;
-        res.send(data.Questions[length - 1]);
-      })
-      .catch(err => {
-        res.send(err);
-      });
-  })
-  .catch((err)=>{
-    res.json(err);
-  });
+  );
+  await Action.findById(ActionID)
+    .then(data => {
+      const length = data.Questions.length;
+      res.json(data.Questions[length - 1]);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 module.exports = router;
