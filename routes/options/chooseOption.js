96ec360cee592b0b9a5c296462361ef70d185de5
mdhishaamakhtar <hishaamakhtar2001.mha@gmail.com>
2020-03-13 21:39:08 +0530

    Update

--- routes/options/chooseOption.js
+++ routes/options/chooseOption.js
@@ -2,15 +2,15 @@
 const mutex = require("locks").createMutex();
 
 const chooseOption = (req, _res) => {
-  const io = req.app.get("socketio");
-  io.on("connection", socket => {
-    socket.on("option", data => {
-      mutex.lock(() => {
-        data.stat += 1;
-        mutex.unlock();
-      });
-      io.emit("option", data);
+    const io = req.app.get("socketio");
+    io.on("connection", socket => {
+        socket.on("option", data => {
+            //mutex.lock(() => {
+            data.stat += 1;
+            //mutex.unlock();
+            //});
+            io.emit("option", data);
+        });
     });
-  });
 };
 module.exports = chooseOption;
