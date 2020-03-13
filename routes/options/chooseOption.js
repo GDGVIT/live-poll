6ec8fa4c21cb0f6dcf878499a8850203fd1c0a18
mdhishaamakhtar <hishaamakhtar2001.mha@gmail.com>
2020-03-13 22:20:29 +0530

    Update

--- routes/options/chooseOption.js
+++ routes/options/chooseOption.js
@@ -4,6 +4,7 @@ const mutex = require("locks").createMutex();
 const chooseOption = (req, _res) => {
     const io = req.app.get("socketio");
     io.on("connection", socket => {
+        console.log("Connected")
         socket.on("option", data => {
             mutex.lock(() => {
                 data.stat += 1;
