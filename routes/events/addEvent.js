83f17d8bf865625da82fd33f4656bd0c38819514
mdhishaamakhtar <hishaamakhtar2001.mha@gmail.com>
2020-03-30 02:10:22 +0530

    Update

--- routes/events/addEvent.js
+++ routes/events/addEvent.js
@@ -5,24 +5,24 @@ const User = require("../../model/User");
 const jwt = require("jsonwebtoken");
 
 const addEvent = async (req, res) => {
-    const token = req.header('auth-token');
-    const decoded = jwt.verify(token, process.env.TOKEN);
-    const options = {
-        min: 100000,
-        max: 999999,
-        integer: true
-    };
-    const event = new Event({
-        Name: req.body.Name,
-        Code: random(options)
-    });
-    const data = await event.save();
-    User.findByIdAndUpdate(decoded._id, {$push: {events: event._id}}, err => {
-        if (err) {
-            return res.json(err)
-        }
-    });
-    res.json(data);
+    try {
+        const token = req.header('auth-token');
+        const decoded = jwt.verify(token, process.env.TOKEN);
+        const options = {
+            min: 100000,
+            max: 999999,
+            integer: true
+        };
+        const event = new Event({
+            Name: req.body.Name,
+            Code: random(options)
+        });
+        const data = await event.save();
+        User.findByIdAndUpdate(decoded._id, {$push: {events: event._id}});
+        res.json(data);
+    } catch (err) {
+        res.json(err);
+    }
 };
 
 module.exports = addEvent;
