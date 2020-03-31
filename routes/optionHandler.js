f218303576808ee591431de0a66bf42e78407454
mdhishaamakhtar <hishaamakhtar2001.mha@gmail.com>
2020-03-31 15:51:19 +0530

    Update

--- routes/optionHandler.js
+++ routes/optionHandler.js
@@ -5,7 +5,7 @@ const verify = require("./verifyToken");
 const addOption = require("./options/addOption");
 const updateStat = require("./options/updateStat");
 
-router.post("/addOption/:ActionID/:QuestionID", verify, addOption);
+router.post("/addOption/:ActionID/:QuestionID", addOption);
 router.post("/updateStat/:ActionID/:QuestionID/:OptionID", verify, updateStat);
 
 module.exports = router;
