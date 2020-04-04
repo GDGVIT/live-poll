f9a489dd65c48c61f6363b15c9a7bae5eba5a1f0
mdhishaamakhtar <hishaamakhtar2001.mha@gmail.com>
2020-04-04 21:38:24 +0530

    Update

--- routes/questionHandler.js
+++ routes/questionHandler.js
@@ -6,10 +6,14 @@ const addQuestion = require("./questions/addQuestion");
 const nextQuestion = require("./questions/nextQuestion");
 const publishQuestion = require("./questions/publishQuestion");
 const closeQuestion = require("./questions/closeQuestion");
+const firstQuestion = require("./questions/firstQuestion");
+const publishFirstQuestion = require("./questions/publishFirstQuestion");
 
 router.post("/addQuestion/:ActionID", verify, addQuestion);
 router.get("/nextQuestion/:ActionID/:QuestionID", nextQuestion);
+router.get("/nextQuestion/:ActionID", firstQuestion);
 router.get("/publishQuestion/:ActionID/:QuestionID", verify, publishQuestion);
+router.get("/publishQuestion/:ActionID", verify, publishFirstQuestion);
 router.get("/closeQuestion/:ActionID/:QuestionID", verify, closeQuestion);
 
 module.exports = router;
---
+++ routes/questions/firstQuestion.js
@@ -0,0 +1,13 @@
+const Action = require("../../model/Action");
+
+const firstQuestion = async(req,res) =>{
+    try{
+        const ActionID = req.params.ActionID;
+        const action = await Action.findById(ActionID);
+        res.send(action.Questions[0]);
+    } catch(err){
+        res.json(err);
+    }
+};
+
+module.exports = firstQuestion;
