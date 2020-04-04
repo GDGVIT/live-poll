3d4591e041e015112bc91edb351ff57b1bf1cea8
mdhishaamakhtar <hishaamakhtar2001.mha@gmail.com>
2020-04-04 18:50:58 +0530

    Update

--- routes/questions/nextQuestion.js
+++ routes/questions/nextQuestion.js
@@ -1,23 +1,22 @@
 //jshint esversion:8
 const Action = require("../../model/Action");
 
-const nextQuestion = (req, res) => {
-  const actionID = req.params.ActionID;
-  Action.findById(actionID)
-    .then(data => {
-      const length = data.Questions.length;
-      let p = 0;
-      for (let i = 0; i < length; i++) {
-        if (data.Questions[i]._id === req.params.QuestionID) {
-          p = i;
-          break;
+const nextQuestion = async (req, res) => {
+    const actionID = req.params.ActionID;
+    try{
+        const data = await Action.findById(actionID);
+        const length = data.Questions.length;
+        let p = 0;
+        for (let i = 0; i < length; i++) {
+            if (data.Questions[i]._id === req.params.QuestionID) {
+                p = i;
+                break;
+            }
         }
-      }
-      res.json(data.Questions[p + 1]);
-    })
-    .catch(err => {
-      res.json(err);
-    });
+        res.json(data.Questions[p + 1]);
+    } catch(err){
+        res.json(err);
+    }
 };
 
 module.exports = nextQuestion;
