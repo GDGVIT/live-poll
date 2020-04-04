84e8378aa681d8c1e449ee9aa85251cfe9b50659
mdhishaamakhtar <hishaamakhtar2001.mha@gmail.com>
2020-04-05 02:31:15 +0530

    Update

---
+++ routes/questions/next.js
@@ -0,0 +1,29 @@
+const Action = require("../../model/Action");
+
+const next = async (req, res) => {
+    try {
+        const ActionID = req.params.ActionID;
+        const QuestionID = req.params.QuestionID;
+        const action = await Action.findById(ActionID);
+        const length = action.Questions.length;
+        let p = 0, flag = 0;
+        for (let i = 0; i < length; i++) {
+            if (action.Questions[i]._id == QuestionID) {
+                p = i;
+                flag = 1;
+                break;
+            }
+        }
+        if (flag === 0) {
+            return res.json({"Error": "Question not found"});
+        }
+        action.Questions[p].isOpen = false;
+        action.Questions[p + 1].isOpen = true;
+        await action.save();
+        res.json(action.Questions[p + 1]);
+    } catch (err) {
+        res.json(err);
+    }
+};
+
+module.exports = next;
