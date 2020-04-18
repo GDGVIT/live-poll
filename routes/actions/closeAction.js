a8d8e0189533e98903dbec78a5c3cf041be208e2
mdhishaamakhtar <hishaamakhtar2001.mha@gmail.com>
2020-04-18 23:35:46 +0530

    Changed close action route to also close all open questions

--- routes/actions/closeAction.js
+++ routes/actions/closeAction.js
@@ -2,13 +2,18 @@ const Action = require("../../model/Action");
 
 const closeAction = async (req, res) => {
     const ActionID = req.params.ActionID;
-    Action.findByIdAndUpdate(ActionID, {$set: {isOpen: false}}, err => {
-        if (err) {
-            res.json(err);
-        } else {
-            res.json({"Status": "Successfully Closed Action"});
+    try {
+        const action = await Action.findById(ActionID);
+        action.isOpen = false;
+        for (let question of action.Questions) {
+            if (question.isOpen == true) {
+                question.isOpen = false;
+            }
         }
-    });
+        res.json({"Status": "Successfully Closed Action"});
+    } catch (err) {
+        res.json(err);
+    }
 };
 
 module.exports = closeAction;
