4df918f9e586db01d59f33077befb9d309b5755a
mdhishaamakhtar <hishaamakhtar2001.mha@gmail.com>
2020-04-24 18:10:38 +0530

    Added route to delete action

--- routes/actionHandler.js
+++ routes/actionHandler.js
@@ -7,11 +7,13 @@ const getActiondetail = require("./actions/getActiondetail");
 const openAction = require("./actions/openAction");
 const closeAction = require("./actions/closeAction");
 const getBasicAction = require("./actions/getBasicAction");
+const deleteAction = require("./actions/deleteAction");
 
 router.post("/addAction/:EventID", verify, addAction);
 router.get("/getActiondetail/:ActionID", getActiondetail);
 router.get("/openAction/:ActionID", openAction);
 router.get("/closeAction/:ActionID", closeAction);
 router.get("/getBasicAction/:ActionID", getBasicAction);
+router.delete("/deleteAction/:ActionID",deleteAction);
 
 module.exports = router;
