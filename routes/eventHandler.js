6e4658f7bcfc5cb66b77927e785277f3e418cece
mdhishaamakhtar <hishaamakhtar2001.mha@gmail.com>
2020-04-25 08:10:02 +0530

    Added update of event title from user history

--- routes/eventHandler.js
+++ routes/eventHandler.js
@@ -7,11 +7,13 @@ const getEventdetail = require("./events/getEventdetail");
 const getEvent = require("./events/getEvent");
 const updateParticipants = require("./events/updateParticipants");
 const deleteEvent = require("./events/deleteEvent");
+const editEventTitle = require("./events/editEventTitle");
 
 router.post("/addEvent", verify, addEvent);
 router.get("/getEventdetail/:EventID", getEventdetail);
 router.get("/getEvent/:Code", getEvent);
 router.get("/updateParticipant/:Code", updateParticipants);
 router.delete("/deleteEvent/:EventID",verify,deleteEvent);
+router.patch("/editEventTitle/:EventID",verify,editEventTitle);
 
 module.exports = router;
---
+++ routes/events/editEventTitle.js
@@ -0,0 +1,13 @@
+const Event = require("../../model/Event");
+
+const editEventTitle = async (req, res) => {
+    try {
+        const title = req.body.title;
+        const eventId = req.params.EventID;
+        await Event.findByIdAndUpdate(eventId, {$set: {title: title}});
+    } catch (err) {
+        res.json(err);
+    }
+}
+
+module.exports = editEventTitle;
