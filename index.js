cc49a3d72ddb0766bbef0efe6af607397b310b1c
mdhishaamakhtar <hishaamakhtar2001.mha@gmail.com>
2020-06-24 02:20:10 +0530

    Cors

--- index.js
+++ index.js
@@ -23,12 +23,7 @@ app.use(
 dotEnv.config();
 app.use(cors({
     origin: function(origin, callback){
-        if(!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) !== -1) {
             callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
     }
 }));
 
