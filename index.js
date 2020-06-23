82dd3ab3cd3bf62ba84032a9ade58258a3af4ff6
mdhishaamakhtar <hishaamakhtar2001.mha@gmail.com>
2020-06-24 00:56:37 +0530

    Pushed a tentative update for fixing CORS in socket.io

--- index.js
+++ index.js
@@ -72,7 +72,17 @@ try {
 } catch (err) {
 	console.info("HTTPS not available");
 }
-const io = socket(server);
+const io = socket(server,{
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Credentials": true
+        };
+        res.writeHead(200, headers);
+        res.end();
+    }
+});
 app.set("socketio", io);
 
 //Calling all routes
